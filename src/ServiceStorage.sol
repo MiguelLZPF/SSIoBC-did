5c383b4caadf23a9840fb588732b9d2c0de091b2
Miguel_LZPF <mgcdreamer@gmail.com>
2024-03-18 18:37:39 +0100

    chore: using library

--- src/ServiceStorage.sol
+++ src/ServiceStorage.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.0 <0.9.0;
 
-import { HashBasedList } from "@src/HashBasedList.sol";
+import { HashBasedList } from "@lib/hash-based-list";
 
 // Example of a service:
 // {
